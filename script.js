d6f93db032433172507059ed14504acc2c6fab81
Samuk-dev1 <stechgames8@gmail.com>
2026-08-04 01:58:21 -0300

    Update JS via Studio PRO

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// Tratamento nativo do botão de voltar para histórico de páginas
+// Adiciona o suporte para o botão voltar do celular navegar no histórico do painel
 window.addEventListener('popstate', function (event) {
-    // Sincroniza o histórico de navegação
+    // Mantém o histórico sincronizado
 });
 
-document.addEventListener('backbutton', function (e) {
-    if (window.history.length > 1) {
-        window.history.back();
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'BrowserBack' || event.keyCode === 8) {
+        if (window.history.length > 1) {
+            window.history.back();
+        }
     }
-}, false);
+});
